7397c5481871046c28d1f8e431e98209bc488f13
saniya123-pal <saniya.palakeeti652@gmail.com>
2026-09-02 12:12:48 +0530

    Create Read_file.py
    creating 2 files , one will be the og file or the input file and the second will show the o/p

--- Read_file.py
+++ Read_file.py
@@ -1,31 +1,19 @@
-# Open APP2.txt in read mode
-File1 = open("APP2.txt", "r")
 
-# Read all lines
-lines = File1.readlines()
+# Read data from APP2.txt
+with open("APP2.txt", "r") as file:
+    lines = file.readlines()
 
-# Close the file
-File1.close()
+# Count the number of lines
+line_count = len(lines)
 
-# Display total number of lines
-print("Total Number of lines in APP2.txt:", len(lines))
-
-# Get first two lines
+# Extract the first two lines
 first_two_lines = lines[:2]
 
-# Display first two lines
-print("\nFirst Two Lines of APP2.txt file:\n")
+# Display line count
+print("Number of lines:", line_count)
 
-for i in first_two_lines:
-    print(i)
+# Write first two lines into op.txt
+with open("op.txt", "w") as file:
+    file.writelines(first_two_lines)
 
-# Open Output.txt in append mode
-File2 = open("Output.txt", "a")
-
-# Write first two lines into Output.txt
-File2.writelines(first_two_lines)
-
-# Close Output.txt
-File2.close()
-
-print("\nFirst two lines from APP2.txt are written in Output.txt")
+print("First two lines written to op.txt")
